680b9b3d0a57c1f432021f4b32b1bd22f356b92e
Ajit Das <ajit.amitav.das@gmail.com>
2014-12-30 10:57:58 -0500

    refactored to add custom exception handler to return NULL in case nothing is configured

--- user/user-core/src/main/java/com/capgemini/user/service/impl/MiscDummyService.java
+++ user/user-core/src/main/java/com/capgemini/user/service/impl/MiscDummyService.java
@@ -7,14 +7,17 @@ import com.capgemini.user.logging.core.Auditable;
 import com.capgemini.user.logging.core.LogPublisher;
 import com.capgemini.user.logging.event.SimpleLogEvent;
 
-@Component("helloDummy")
-public class HelloDummy {
+@Component("miscDummyService")
+public class MiscDummyService {
 	
 	private static final LogPublisher logPublisher = AppContextSingleton.getInstance().getLogPublisher();
 
-	@Auditable
-	public void sayHello(){
-		logPublisher.publishLog(new SimpleLogEvent("Hello Dummy Service Bean", "TRACE"));
+	public void greetHelloWithLog(){
+		logPublisher.publishLog(new SimpleLogEvent("Hello Service Bean Log", "TRACE"));
 	}
 	
+	@Auditable
+	public void greetHelloWithLogAlongWithAudit(){
+		logPublisher.publishLog(new SimpleLogEvent("Hello Service Bean Audit Log", "TRACE"));
+	}
 }
